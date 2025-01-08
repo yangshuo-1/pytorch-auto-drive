f186f8d58ed4ce57f3625613a20d2cd8d323e971
yangxy <yangxy012@gmail.com>
2025-01-08 11:39:18 +0800

    feat: ca代码+测试

---
+++ utils/models/common_models/module/ca.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)          # 缩减通道 
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+        
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        
+
+    def forward(self, x):
+        identity = x
+        
+        # 对输入特征图进行池化得到高度和宽度的特征 
+        n,c,h,w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        # h维度上进行拼接然后处理 
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)                       # 拼接后的特征进行融合和通道缩减 
+        y = self.bn1(y)
+        y = self.act(y) 
+        
+        # 分割
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        # 计算注意力权重
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+
+        return out
+if __name__ == '__main__':
+    img = torch.randn(2, 256, 18, 50)
+    net = CoordAtt(inp=256, oup=256)
+    print(net)
+    out = net(img)
+    print(out.size())
